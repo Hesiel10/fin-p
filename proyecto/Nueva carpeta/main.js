8727d5e59a05299bc214a418cf34ea630c2cb57c
Hesiel10 <135052811+Hesiel10@users.noreply.github.com>
2023-06-12 21:43:45 -0400

    Add files via upload

---
+++ proyecto/Nueva carpeta/main.js
@@ -0,0 +1,6 @@
+function agregarAlCarrito(tipo) {
+    const carrito = document.getElementById('carrito');
+    const li = document.createElement('li');
+    li.textContent = tipo;
+    carrito.appendChild(li);
+}
